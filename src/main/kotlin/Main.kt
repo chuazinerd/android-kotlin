b48e3f2e6e999e730bf6af06b1fff3a95e2924cb
Felipe Figueiredo <recoverxfj@gmail.com>
2025-10-15 16:39:25 -0300

    Melhorando explicação sobre as diferentes funções

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,10 @@ fun main() {
     val frase2 = "é"
     val frase3 = "top demais"
     val polando = "polando"
+    //Para mostrar o retorno dos valores das variáveis
+    //"println(frase1 + frase2 + frase3)" deve ser usar o operador de concatenação "+",
+    //no entanto é possivel usar o operador de interpolação "$".
     println("$inter =====$frase1 $frase2 $frase3===== $polando")
-
     println("Mostra valor máximo: ${Double.MAX_VALUE}")
+    //"$" é para variáveis, e ${} é para o encapsulamento de tudo que é mais complexo que uma variável.
 }
