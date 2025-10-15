d15c6719e24bcfd2e5d63a3ddbe2d859bd03837a
Felipe Figueiredo <recoverxfj@gmail.com>
2025-10-15 16:01:19 -0300

    Valores que podem e não podem ser alterados

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("${Double.MAX_VALUE} - ${Double.MIN_VALUE}")
+    // str: é o mesmo que: str será do tipo String
+    var str: String = "abc"//É possivel usar var, mas não aconselhavel porque no fututo ela pode ser alterada
+    //o que causará erro
+    println(str)
+    str  = "jde"//Valor alterado dentro de str
+
+    //Forma mais segura:
+    val texto: String = "abc"//Com val o valor armazenado é definitivo
+    val = "jdk"// <- Aqui a alteração não foi aceita, o que era esperado
 }
