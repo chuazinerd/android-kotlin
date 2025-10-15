36c0096492032ba8a83e0d54e1e035b601238ee3
Felipe Figueiredo <recoverxfj@gmail.com>
2025-10-15 13:50:52 -0300

    Comentários no Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
-//Primeira função em Kotlin
+/*
+ * Tipos de comentário para muitas linhas
+ */
+/**
+ * Documentação
+ * */
+//Comentario de linha unica
+//Ponto de entrada
 fun main() {
-    println("Hello, Android")
+    println("Hello, Android")//Tipos de comentário
 }
