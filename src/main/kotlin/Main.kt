18bee18da051e8490fed8a5ea8a787ff117708aa
Felipe Figueiredo <recoverxfj@gmail.com>
2025-10-15 16:26:52 -0300

    Misturando valores com interpolação

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,10 @@
 fun main() {
-    // str: é o mesmo que: str será do tipo String
-    var str: String = "abc"//É possivel usar var, mas não aconselhavel porque no fututo ela pode ser alterada
-    //o que causará erro
-    println(str)
-    str  = "jde"//Valor alterado dentro de str
+    val inter = "Inter"
+    val frase1 = "Kotlin"
+    val frase2 = "é"
+    val frase3 = "top demais"
+    val polando = "polando"
+    println("$inter =====$frase1 $frase2 $frase3===== $polando")
 
-    //Forma mais segura:
-    val texto: String = "abc"//Com val o valor armazenado é definitivo
-    val = "jdk"// <- Aqui a alteração não foi aceita, o que era esperado
+    println("Mostra valor máximo: ${Double.MAX_VALUE}")
 }
