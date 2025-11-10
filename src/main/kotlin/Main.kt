aba63099da8326261e5dc80751dc12a51a4b7fe5
Felipe Figueiredo <recoverxfj@gmail.com>
2025-11-10 08:57:58 -0300

    Capturando dados do usuário

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,5 @@
 fun main() {
-    val nome = "Jose";
-    println("Olá, $nome");
-    println("Olá, %s".format(nome));//Útil quando  é preciso de uma formatação muito específica
-    // (como número de casas decimais,
-    // %s - String - cadeia de caracteres
-    // %d - Int - inteiro
-    // %f - Ponto flutuante
-    // %c - char - caractere
-    // %b - boleano - verdadeiro ou falso
-    val value= 5;
-    val salario = 15898.34
-    println("Valor: %05d - salário: %.3f".format(value, salario));//%d formata value para inteiro
-    // enquanto %.3f formata para tipo flutuante com 3 casas decimais
+    print("Informe um valor: ");
+    val valor =  readln();//Para pegar o que o usuário digitar
+    println(valor);
 }
