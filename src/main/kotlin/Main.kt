c8cdc9eb5c58ac9a06f747f48de278ac8bf32947
Felipe Figueiredo <recoverxfj@gmail.com>
2025-11-10 08:42:15 -0300

    Formatando valores

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,15 @@
 fun main() {
-    val texto = "Kotlin -> Android"
-
-    println(texto[0])//Mostra a primeira posição do texto
-    println(texto.length)//Apresenta o total de caracteres
-    println(texto.startsWith("z"))//Verifica se texto começa c/ "z" e retorna verdadeiro ou falso
-    println(texto.endsWith("A"))//Tambem verifica se texto termina c/ "z" e retorna verdadeiro ou falso
-    println(texto.substring(16))//Mostra apartir do caractere n 16
-    println(texto.substring(4, 12))//Retorna do caractere 5 até o caractere 11
-    println(texto.replace("o" , "0"))//Procura o caractere escolhido e o substitui
-
-    println(texto.uppercase())//Deixa o texto em caixa alta
-    println(texto.lowercase())//Deixa o texto em caixa baixa
-    println(texto.contains("Android"))//Retorna se existe ou não "Android"
-    texto.isEmpty()
-    println("  asdfasdf asdfasdf asdfasdf ".trim())//Remove os espaços dos cantos
+    val nome = "Jose";
+    println("Olá, $nome");
+    println("Olá, %s".format(nome));//Útil quando  é preciso de uma formatação muito específica
+    // (como número de casas decimais,
+    // %s - String - cadeia de caracteres
+    // %d - Int - inteiro
+    // %f - Ponto flutuante
+    // %c - char - caractere
+    // %b - boleano - verdadeiro ou falso
+    val value= 5;
+    val salario = 15898.34
+    println("Valor: %05d - salário: %.3f".format(value, salario));//%d formata value para inteiro
+    // enquanto %.3f formata para tipo flutuante com 3 casas decimais
 }
